392d425a6e2bb6c74ccc219c1f38ea7907bdfebe
dalorkee <31229188+dalorkee@users.noreply.github.com>
2018-02-16 15:33:58 +0700

    change site title

--- resources/views/layouts/templateDemo-graph.blade.php
+++ resources/views/layouts/templateDemo-graph.blade.php
@@ -6,7 +6,7 @@ $pj = 'PJ';
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>CEI Project</title>
+	<title>IUDC Report</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	@include('layouts.incHeaderScript')
