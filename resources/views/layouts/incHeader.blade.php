31d87a61fa095ce893c61f3e40507c3649490ebe
dalorkee <31229188+dalorkee@users.noreply.github.com>
2018-02-16 15:26:56 +0700

    change Site Name

--- resources/views/layouts/incHeader.blade.php
+++ resources/views/layouts/incHeader.blade.php
@@ -1,9 +1,9 @@
 <!-- Logo -->
 <a href="/" class="logo">
 	<!-- mini logo for sidebar mini 50x50 pixels -->
-	<span class="logo-mini"><b>C</b>PJ</span>
+	<span class="logo-mini"><b>C</b>IUDCR</span>
 	<!-- logo for regular state and mobile devices -->
-	<span class="logo-lg"><b>CEI</b>Project</span>
+	<span class="logo-lg"><b>Iudc</b>Report</span>
 </a>
 <!-- Header Navbar: style can be found in header.less -->
 <nav class="navbar navbar-static-top">
@@ -96,13 +96,13 @@
 			<li class="dropdown user user-menu">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 					{{ Html::image('public/AdminLTE-2.4.2/dist/img/user2-160x160.jpg', 'alt=User Image', array('class'=>'user-image')) }}
-					<span class="hidden-xs">Alexander Pierce</span>
+					<span class="hidden-xs">Iudc001</span>
 				</a>
 				<ul class="dropdown-menu">
 					<!-- User image -->
 					<li class="user-header">
 						{{ Html::image('public/AdminLTE-2.4.2/dist/img/user2-160x160.jpg', 'alt=User Image', array('class'=>'img-circle')) }}
-						<p>Alexander Pierce - Web Developer<small>Member since Nov. 2012</small></p>
+						<p>Iudc001 - Web Developer<small>Member since Nov. 2012</small></p>
 					</li>
 					<!-- Menu Body -->
 					<li class="user-body">
