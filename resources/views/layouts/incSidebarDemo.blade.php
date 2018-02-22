11027098192c659ee9da864ecc98b402cb9c8abf
DESKTOP-P58EKRP\Sataphat <sataphat.dev@gmail.com>
2018-02-22 12:53:23 +0700

    edit left menu for select year / add parameter select year to export menu

--- resources/views/layouts/incSidebarDemo.blade.php
+++ resources/views/layouts/incSidebarDemo.blade.php
@@ -5,12 +5,30 @@
 		<li class="header">MAIN NAVIGATION</li>
 		<li class="treeview">
 			<a href="#">
-				<i class="fa fa-dashboard"></i> <span>Demo</span>
+				<i class="fa fa-calendar"></i> <span>เลือกปี</span>
+				<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
+			</a>
+			<ul class="treeview-menu" style="display: block;">
+				<?php
+							$current_year =  (isset($_GET['year']))? $_GET['year']: date('Y');
+							//Current Year
+							$already_selected_value = (int)date('Y');
+					    //Start Year
+							$earliest_year = 2012;
+							foreach (range($already_selected_value, $earliest_year) as $x) {
+									 $year_th = $x+543;
+									 echo '<li '.($x === (int)$current_year ? 'class="active"' : '').'><a href="/?year='.$x.'"><i class="fa fa-circle-o"></i>'.$year_th.'</a></li>';
+							}
+							?>
+			</ul>
+		</li>
+		<li class="treeview">
+			<a href="#">
+				<i class="fa  fa-download"></i> <span>Export</span>
 				<span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
 			</a>
 			<ul class="treeview-menu">
-			    <li><a href="{{ route('dashboard') }}"><i class="fa fa-circle-o"></i> Population</a></li>
-				<li><a href="{{ route('export.form') }}"><i class="fa fa-circle-o"></i> Export</a></li>
+				<li><a href="{{ route('export.form') }}?year=<?php echo $current_year;?>"><i class="fa fa-circle-o"></i> Export File</a></li>
 			</ul>
 		</li>
 	</ul>
