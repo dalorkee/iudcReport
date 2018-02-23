7426c6cae024c0d58a1ce4ee32d0804eca4e58d8
DESKTOP-P58EKRP\Sataphat <sataphat.dev@gmail.com>
2018-02-23 08:33:10 +0700

    add render time

--- resources/views/layouts/incFooter.blade.php
+++ resources/views/layouts/incFooter.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">
-		<b>Version</b> 1.0.0
+		<b>Version</b> 1.0.0 Page Speed This page took {{ number_format(microtime(true) - LARAVEL_START,1) }} seconds to render 
 	</div>
 	<strong>Copyright &copy; 2017 <a href="https://adminlte.io">CEI Studio</a>.</strong> All rights reserved.
 </footer>
