20176e737751c00b34292e7123632f8143cd049a
DESKTOP-L2M3PNA\SATAPHAT-DEV <sataphat.dev@gmail.com>
2018-02-25 15:11:16 +0700

    add loading animation

--- resources/views/layouts/incStylesheet.blade.php
+++ resources/views/layouts/incStylesheet.blade.php
@@ -25,4 +25,13 @@
         left:-17px;
         cursor: pointer;
     }
+    .loader {
+    	position: fixed;
+    	left: 0px;
+    	top: 0px;
+    	width: 100%;
+    	height: 100%;
+    	z-index: 9999;
+    	background: url({{ URL::asset('public/images/Blocks.gif') }}) 50% 50% no-repeat rgb(249,249,249);
+    }
 </style>
