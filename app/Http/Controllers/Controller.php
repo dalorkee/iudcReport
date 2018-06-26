f6613ad26ff3d0502d5568a1a9b8f3346d8c679a
DESKTOP-P58EKRP\Sataphat <sataphat.dev@gmail.com>
2018-06-26 15:59:28 +0700

    add function dpc

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -61,9 +61,6 @@ class Controller extends BaseController
     }
     //list sector
     public static function get_pop_sector($sector){
-      //if(empty($sector)) return false;
-      //echo $sector;
-      //exit;
 
       if($sector=="north-region"){
         $array_sector = array("50","51","52","58","54","55","56","57","53","63","64","65","67","60","61","62","66");
@@ -73,8 +70,10 @@ class Controller extends BaseController
         $array_sector = array("40","44","45","46","38","39","41","42","43","47","48","30","31","32","36","33","34","35","37","49");
       }elseif($sector == "southern-region"){
         $array_sector = array("80","81","82","83","84","85","86","90","91","91","93","94","95","96");
-      }else{
+      }elseif($sector == "all-region"){
         $array_sector = array("50","51","52","58","54","55","56","57","53","63","64","65","67","60","61","62","66","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","70","71","72","73","74","75","76","77","40","44","45","46","38","39","41","42","43","47","48","30","31","32","36","33","34","35","37","49","80","81","82","83","84","85","86","90","91","91","93","94","95","96");
+      }else{
+        $array_sector = array();
       }
         return $array_sector;
     }
@@ -87,4 +86,52 @@ class Controller extends BaseController
                           "southern-region" => "ภาคใต้");
         return $array_name_th;
     }
+    //DPC code
+    public static function get_pop_dpc($dpc_code){
+      if($dpc_code=='01'){
+          $array_dpc_code = array('50','51','52','54','55','56','57','58');
+      }elseif($dpc_code=='02'){
+          $array_dpc_code = array('53','63','64','65','67');
+      }elseif($dpc_code=='03'){
+          $array_dpc_code = array('18','60','61','62','66');
+      }elseif($dpc_code=='04'){
+         $array_dpc_code = array('12','13','14','15','16','17','19','26');
+      }elseif($dpc_code=='05'){
+         $array_dpc_code = array('70','71','72','73','74','75','76','77');
+      }elseif($dpc_code=='06'){
+         $array_dpc_code = array('11','20','21','22','23','24','25','27');
+      }elseif($dpc_code=='07'){
+         $array_dpc_code = array('40','44','45','46');
+      }elseif($dpc_code=='08'){
+         $array_dpc_code = array('38','39','41','42','43','47','48');
+      }elseif($dpc_code=='09'){
+         $array_dpc_code = array('30','31','32','36');
+      }elseif($dpc_code=='10'){
+         $array_dpc_code = array('33','34','35','37','49');
+      }elseif($dpc_code=='11'){
+         $array_dpc_code = array('80','81','82','83','84','85','86');
+      }elseif($dpc_code=='12'){
+         $array_dpc_code = array('90','91','92','93','94','95','96');
+      }elseif($dpc_code=='13'){
+         $array_dpc_code = array('10');
+      }
+    }
+    //DCP Name
+    public static function get_pop_dpc_nameth(){
+      return array('01' => 'สคร.1',
+                   '02' => 'สคร.2',
+                   '03' => 'สคร.3',
+                   '04' => 'สคร.4',
+                   '05' => 'สคร.5',
+                   '06' => 'สคร.6',
+                   '07' => 'สคร.7',
+                   '08' => 'สคร.8',
+                   '09' => 'สคร.9',
+                   '10' => 'สคร.10',
+                   '11' => 'สคร.11',
+                   '12' => 'สคร.12',
+                   '13' => 'สปคม.',
+                 );
+    }
+
 }
