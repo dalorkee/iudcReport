8c704cfbd68ee973318877b044faeda98d815395
DESKTOP-P58EKRP\Sataphat <sataphat.dev@gmail.com>
2018-06-26 15:07:19 +0700

    add model tbl pop_urban_age

---
+++ app/PopulationUrbanSex.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PopulationUrbanSex extends Model
+{
+    protected $table = "pop_urban_age";
+    public $timestamps = false;
+    //public $fillable = ['DISEASE','DISNAME','total','gr'];
+}
