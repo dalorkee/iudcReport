bbcc82195f5856ef60718de8329a5cc27fadd063
DESKTOP-L2M3PNA\SATAPHAT-DEV <sataphat.dev@gmail.com>
2018-08-02 14:46:51 +0700

    edit color hover table

--- resources/views/frontend/patient-sick-death-ratio.blade.php
+++ resources/views/frontend/patient-sick-death-ratio.blade.php
@@ -198,7 +198,7 @@ $(document).ready(function() {
             api.column(groupColumn, {page:'current'} ).data().each( function ( group, i ) {
                 if ( last !== group ) {
                     $(rows).eq( i ).before(
-                        '<tr class="group"><td colspan="27">'+group+'</td></tr>'
+                        '<tr class="group"><td colspan="7">'+group+'</td></tr>'
                     );
                     last = group;
                 }
